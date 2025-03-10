ab743757a8c5708d20dbfbb261be79241434b8c4
Jordan Andrzejczak <jordan.andrzejczak@allegro.com>
2025-03-10 09:46:29 +0100

    ✨ feat: add E2E tests configuration

---
+++ e2e/example.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page, baseURL }) => {
+  await page.goto(baseURL!);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/tapnosh/);
+});
