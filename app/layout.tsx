cc75bbcccf51b6f71551de11af641cf14e28dc69
Jordan Andrzejczak <jordan.andrzejczak@allegro.com>
2025-03-10 21:11:50 +0100

    📝 docs: move docs and fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { getLocale, getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
-import "./globals.css";
+import { Head as NextraHead } from "nextra/components";
+import "../assets/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,13 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} dir="ltr">
+      <NextraHead
+        color={{
+          hue: { dark: 28, light: 357 },
+          saturation: { dark: 100, light: 18 },
+          lightness: { dark: 92, light: 40 },
+        }}
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
