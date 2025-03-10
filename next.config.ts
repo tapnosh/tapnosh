5cf6f32c75d4f06b679734696cf55c934066e884
Jordan Andrzejczak <jordan.andrzejczak@allegro.com>
2025-03-10 01:33:02 +0100

    📝 docs(create folder and files structure for documentation): create structure for documentation (#5)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import nextra from "nextra";
 
-const withNextra = nextra({});
-
-export default withNextra({
+const withNextra = nextra({
   contentDirBasePath: "/docs",
 });
+
+export default withNextra({});
