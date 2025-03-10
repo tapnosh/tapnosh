062905b0b17fc6a6863321a83026142c092d770c
Jordan Andrzejczak <jordan.andrzejczak@allegro.com>
2025-03-10 10:17:39 +0100

    🐛 fix: fix linter

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { LocaleSwitcher } from "@/components/LocaleSwitcher";
-import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
