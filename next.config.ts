44785d7c63ad88cfa3a793e1891ea4a7dde7e0af
Jordan Andrzejczak <jordan.andrzejczak@allegro.com>
2025-03-10 21:11:50 +0100

    📝 docs: move docs and fix layout

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
+import nextra from "nextra";
 import createNextIntlPlugin from "next-intl/plugin";
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
 
+const withNextra = nextra({
+  latex: true,
+  search: {
+    codeblocks: true,
+  },
+  contentDirBasePath: "/docs",
+});
+
 const withNextIntl = createNextIntlPlugin("./translations/config.ts");
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(withNextra(nextConfig));
