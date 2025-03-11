6de80938526d6a89769400d63c55fdb33e135d0a
Jordan Andrzejczak <jordan.andrzejczak@allegro.com>
2025-03-11 20:38:34 +0100

    🐛 fix: close menu on mobile when item clicked

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -499,6 +499,7 @@ function SidebarMenuButton({
   size = "default",
   tooltip,
   className,
+  onClick,
   ...props
 }: React.ComponentProps<"button"> & {
   asChild?: boolean;
@@ -506,7 +507,14 @@ function SidebarMenuButton({
   tooltip?: string | React.ComponentProps<typeof TooltipContent>;
 } & VariantProps<typeof sidebarMenuButtonVariants>) {
   const Comp = asChild ? Slot : "button";
-  const { isMobile, state } = useSidebar();
+  const { isMobile, state, setOpenMobile } = useSidebar();
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onClick?.(event);
+    if (isMobile) {
+      setOpenMobile(false);
+    }
+  };
 
   const button = (
     <Comp
@@ -515,6 +523,7 @@ function SidebarMenuButton({
       data-size={size}
       data-active={isActive}
       className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
+      onClick={handleClick}
       {...props}
     />
   );
