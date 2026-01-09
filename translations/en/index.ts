7c09efceb00bcbfb9d5c9a49cce2acea7903f1b5
Filip M. <34985691+popa3@users.noreply.github.com>
2026-01-09 06:18:33 +0100

    ✨ feat(i18n): update translation infrastructure with new namespaces (re #223)
    re #223

--- translations/en/index.ts
+++ translations/en/index.ts
@@ -1,7 +1,41 @@
 import categories from "./categories.json";
+import common from "./common.json";
+import home from "./home.json";
+import dashboard from "./management/dashboard.json";
+import pageBuilder from "./management/page-builder.json";
+import scannableMenu from "./management/scannable-menu.json";
+import settings from "./management/settings.json";
+import map from "./map.json";
+import navigation from "./navigation.json";
+import restaurantBrowse from "./restaurants/browse.json";
+import restaurantDetails from "./restaurants/details.json";
+import restaurantFormErrors from "./restaurants/form/errors.json";
+import restaurantFormFields from "./restaurants/form/fields.json";
+import restaurantLists from "./restaurants/lists.json";
 
 const en = {
   categories,
+  common,
+  home,
+  navigation,
+  map,
+
+  management: {
+    pageBuilder,
+    scannableMenu,
+    settings,
+    dashboard,
+  },
+
+  restaurants: {
+    browse: restaurantBrowse,
+    details: restaurantDetails,
+    lists: restaurantLists,
+    form: {
+      errors: restaurantFormErrors,
+      fields: restaurantFormFields,
+    },
+  },
 };
 
 export default en;
